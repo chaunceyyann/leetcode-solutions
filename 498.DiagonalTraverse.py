936bafe99324a2fccc0055f07e1bcf05d705c2f3
cyanjin <chaunceyyann@gmail.com>
2017-07-17 20:06:57 -0700

    Added solved 498

--- 498.DiagonalTraverse.py
+++ 498.DiagonalTraverse.py
@@ -1,43 +1,37 @@
 class Solution(object):
     def findDiagonalOrder(self, matrix):
-        direction="u"
-        i,j=0,0
-        if matrix:
-            rtn=[matrix[i][j]]
-            M=len(matrix)
-            N=len(matrix[0])
-            while len(rtn)<=M*N:
-
-                if i==M-1 and j==N-1:
-                    break
-                if direction=="u":
-                    if i-1<0 and j+1<N:
-                        direction="d"
-                        j=j+1
-                    elif j+1>=N:
-                        i=i+1
-                        direction="d"
-                    else :
-                        i=i-1
-                        j=j+1
-                
-                        
-                elif direction=="d":
-                    if j-1<0 and i+1<M:
-                        direction="u"
-                        i=i+1
-                    elif i+1>=M:
-                        j=j+1
-                        direction="u"
-                    else :
-                        i=i+1
-                        j=j-1
-                #print "ij",i,j
-                #print "d",direction
-                #print matrix[i][j]
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        if not matrix:
+            return [] 
+        else :
+            rtn=[matrix[0][0]]
+            m=len(matrix)
+            n=len(matrix[0])
+            i,j=0,0 # m,i -> rows . n,j -> colums
+            di=0    # 0 -> up, 1 -> down 
+            while len(rtn)<m*n:
+                if not di:
+                    di=1
+                    if i-1<0 and j+1<n:     #case1
+                        j+=1
+                    elif j+1>=n:            #case2
+                        i+=1
+                    else :                  #normal
+                        i-=1
+                        j+=1
+                        di=0
+                else:
+                    di=0
+                    if j-1<0 and i+1<m:     #case3
+                        i+=1
+                    elif i+1>=m:            #case 4
+                        j+=1
+                    else :                  #normal
+                        i+=1
+                        j-=1
+                        di=1
                 rtn.append(matrix[i][j])
-                
-                
             return rtn
-        else:
-            return []
